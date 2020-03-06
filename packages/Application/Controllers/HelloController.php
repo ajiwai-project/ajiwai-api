0cc130fb63c46e849ab6e0b1e2ea26f6591c0403
t-okada <t-okada@atware.co.jp>
2020-03-06 16:29:15 +0900

    init project

---
+++ packages/Application/Controllers/HelloController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Ajiwai\Application\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class HelloController extends Controller
+{
+    public function index()
+    {
+      return 'hello world';
+    }
+}
